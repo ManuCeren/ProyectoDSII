c1d1256246ac9a6f84b4e4820527bb8561418067
ManuCeren <manu.ceren87@gmail.com>
2025-05-19 22:43:30 -0600

    Create EnviosController.cs
    API envios

---
+++ webApiProyectoDSII/WebApiProyectoDSII/Controllers/EnviosController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiProyectoDSII.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApiProyectoDSII.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvioController : ControllerBase
+    {
+        private readonly TransporteFloresDbContext dbContext;
+        public EnvioController(TransporteFloresDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Get()
+        {
+            var listaEnvios = await dbContext.Envíos.ToListAsync();
+            
+            return StatusCode(StatusCodes.Status200OK, listaEnvios);
+        }
+        [HttpGet]
+        [Route("Obtener/{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var envio = await dbContext.Envíos.FirstOrDefaultAsync(c => c.IdEnvios == id);
+            return StatusCode(StatusCodes.Status200OK, envio);
+        }
+
+        [HttpPost]
+        [Route("Nuevo")]
+        public async Task<IActionResult> Nuevo([FromBody] Envío objeto)
+        {
+            await dbContext.Envíos.AddAsync(objeto);
+            await dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody] Envío objeto)
+        {
+            dbContext.Envíos.Update(objeto);
+            await dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var envio = await dbContext.Envíos.FirstOrDefaultAsync(c => c.IdEnvios == id);
+            dbContext.Envíos.Remove(envio);
+            await dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+        }
+    }
+}
